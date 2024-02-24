213b4262e1f72e7ac9d0ee6e2b4de0265909e52e
keosambath <keosambathnov@gmail.com>
2024-02-24 15:29:12 +0700

    add logic to billing system

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,29 @@ class Customer{
    private int oldmeternumber;
    private int newEnergyUse;
    private int oldEnergyUse;
+   private int summeterwater;
+   private int sumEnergyUsed;
+   private int priceofwater;
+   private int priceofEnergy;
+   private int sumElectric;
+   private int sumWater;
+   private int total;
 
-   public void input(){
+    public Customer(int oldmeternumber, int oldEnergyUse, int priceofEnergy, int priceofwater) {
+        this.oldmeternumber = oldmeternumber;
+        this.oldEnergyUse = oldEnergyUse;
+        this.priceofEnergy = priceofEnergy;
+        this.priceofwater =priceofwater;
+    }
+
+    public void input(){
        System.out.println("Enter your ID first :");
        id = ent.nextInt();
+       ent.nextLine();
        System.out.println("Enter Customer address:");
        address= ent.nextLine();
-       System.out.println("Enter Water meter use last month:");
-       oldmeternumber = ent.nextInt();
        System.out.println("Enter Water meter use new month:");
        newmeternumber = ent.nextInt();
-
-       System.out.println("Enter your Last month energy use:");
-       oldEnergyUse = ent.nextInt();
        System.out.println("Enter your New month energy use :");
        newEnergyUse = ent.nextInt();
 
@@ -28,18 +38,49 @@ class Customer{
     public void output(){
         System.out.println("Customer ID:"+ id);
         System.out.println("Customer Address: " + address);
-        System.out.println("Customer Address: " + oldmeternumber);
-        System.out.println("Customer Address: " + newmeternumber);
-        System.out.println("Customer Address: " + oldEnergyUse);
-        System.out.println("Customer Address: " + newEnergyUse);
+        System.out.println("Customer oldmeterNumber: " + oldmeternumber);
+        System.out.println("Customer NewmeterNumber: " + newmeternumber);
+        System.out.println("Customer OldEnergyNumber: " + oldEnergyUse);
+        System.out.println("Customer NewEnergyNumber: " + newEnergyUse);
+        System.out.println("Customer Calculate Water Meter Used: " + CalWaterMeter());
+        System.out.println("Customer Calculate ElectricEnergy Used: " + calElectricEnergy());
+        System.out.println("Customer Total Electricbill: " + ElectricPrice());
+        System.out.println("Customer Total WaterBill: " + WaterMeterPrice());
+        System.out.println("Customer Total Bill this month: " + RenterBill());
     }
 
+    public int CalWaterMeter(){
+        return summeterwater = newmeternumber - oldmeternumber;
+    }
+
+    public int calElectricEnergy(){
+        return sumEnergyUsed = newEnergyUse - oldEnergyUse;
+
+    }
+
+    public int ElectricPrice(){
+        return sumElectric = sumEnergyUsed * priceofEnergy;
+    }
+
+    public int WaterMeterPrice(){
+        return sumWater = summeterwater  *priceofwater;
+    }
+
+    public int RenterBill( ){
+        return total = sumElectric + sumWater;
+    }
 
 }
-
-
 public class Main {
     public static void main(String[] args) {
+        Customer renter = new Customer(917, 7511, 700, 1000);
+        System.out.println("Welcome to Saren Electric - Water Billing System-->");
+        System.out.println("Please fill the information here-->");
+        renter.input();
+        System.out.println("Here is this month bill-->");
+        renter.output();
+
+
 
     }
 }
